45dfb6d075bbed7df20250629f2f430d6a74df63
Matthias Welwarsky <matthias.welwarsky@sysgo.com>
2016-09-03 17:12:18 +0200

    target: add -ctibase config option in addition to -dbgbase
    Some vendors don't fully populate the ROM table, e.g. BCM2357 (used in
    Raspberry Pi 3) doesn't list CTI, however it is mandatory for halting
    an ARMv8 core and therefore it's always present (and required),
    regardless of the ROM table listing it or not.
    
    Change-Id: Ia18a4f1b5b931ccd19805b188ebf737c837c6b54
    Signed-off-by: Matthias Welwarsky <matthias.welwarsky@sysgo.com>

--- src/target/target.c
+++ src/target/target.c
@@ -4344,6 +4344,7 @@ enum target_cfg_param {
 	TCFG_COREID,
 	TCFG_CHAIN_POSITION,
 	TCFG_DBGBASE,
+	TCFG_CTIBASE,
 	TCFG_RTOS,
 	TCFG_DEFER_EXAMINE,
 };
@@ -4359,6 +4360,7 @@ static Jim_Nvp nvp_config_opts[] = {
 	{ .name = "-coreid",           .value = TCFG_COREID },
 	{ .name = "-chain-position",   .value = TCFG_CHAIN_POSITION },
 	{ .name = "-dbgbase",          .value = TCFG_DBGBASE },
+	{ .name = "-ctibase",          .value = TCFG_CTIBASE },
 	{ .name = "-rtos",             .value = TCFG_RTOS },
 	{ .name = "-defer-examine",    .value = TCFG_DEFER_EXAMINE },
 	{ .name = NULL, .value = -1 }
@@ -4625,7 +4627,20 @@ no_params:
 			Jim_SetResult(goi->interp, Jim_NewIntObj(goi->interp, target->dbgbase));
 			/* loop for more */
 			break;
-
+		case TCFG_CTIBASE:
+			if (goi->isconfigure) {
+				e = Jim_GetOpt_Wide(goi, &w);
+				if (e != JIM_OK)
+					return e;
+				target->ctibase = (uint32_t)w;
+				target->ctibase_set = true;
+			} else {
+				if (goi->argc != 0)
+					goto no_params;
+			}
+			Jim_SetResult(goi->interp, Jim_NewIntObj(goi->interp, target->ctibase));
+			/* loop for more */
+			break;
 		case TCFG_RTOS:
 			/* RTOS */
 			{
